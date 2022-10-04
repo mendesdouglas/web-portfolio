9081d30eeff6b40973ee75d96dc3ca859af08442
Douglas Mendes <lixao_9@hotmail.com>
2022-10-04 20:14:56 -0300

    feat: inserting intro, image and fonts

--- src/components/intro/Intro.tsx
+++ src/components/intro/Intro.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
 import './intro.scss'
-
+import hero from '../../assets/hero.png'
 export default function Intro() {
   return (
-    <div className='intro' id='intro'>Intro</div>
+    <div className='intro' id='intro'>
+      <div className="left">
+        <div className="imgContainer">
+          <img src={hero} alt="" />
+        </div>
+        </div>
+        
+        <div className="right">
+          <div className="wrapper">
+            <h2>Olá, Eu sou</h2>
+            <h1>Douglas Mendes</h1>
+            <h3>Desenvolvedor Front e Back-end <span></span></h3>
+          </div>
+        
+      
+      </div>
+    </div>
   )
 }
